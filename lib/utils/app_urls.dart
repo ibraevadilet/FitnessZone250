ad72536357cc0363d0e59055624863fb2bb8b207
Ravshan <kasimovravshan03@gmail.com>
2024-01-15 19:58:20 +0600

    add links

--- lib/utils/app_urls.dart
+++ lib/utils/app_urls.dart
@@ -1,6 +1,6 @@
 class AppUrlsFitnessZone {
-  static const String aphKey = '';
-  static const String prPol = 'https://pub.dev/';
-  static const String terUSe = 'https://pub.dev/';
-  static const String supp = 'https://pub.dev/';
+  static const String aphKey = 'app_K55x4mpZBXuvLoDpYYKmRU91Kjw2sP';
+  static const String prPol = 'https://docs.google.com/document/d/1ljAwx8OvSnMKVmF8de5NN5ZuX37DQHHQeoNe823GeSI/edit?usp=sharing';
+  static const String terUSe = 'https://docs.google.com/document/d/1Opfwv_SiQRuSz6G8RQjd52i6z0lgl4dDkZenroynSR8/edit?usp=sharing';
+  static const String supp = 'https://forms.gle/2G4dGUoaj8CB6PvUA';
 }
